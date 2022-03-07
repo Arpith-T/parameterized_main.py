2abf8e77fa2d932a84cedb161c2cf2d58eaa1c94
Arpith-T <40532486+Arpith-T@users.noreply.github.com>
2022-03-07 08:31:29 +0530

    added mapping and instance state

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import time
 from multiprocessing import Pool
 import random
 
-LATENCY = int(os.getenv("LATENCY"))
-DURATION = int(os.getenv("DURATION"))
+
+# LATENCY = int(os.getenv("LATENCY"))
+# DURATION = int(os.getenv("DURATION"))
+# app_string = os.getenv("CF_Microservice")
+# app_list = app_string.split(',')
+# Chaos_Action = os.getenv("Chaos_Action")
+# PASSWORD = os.getenv("PASSWORD")
 # az = os.getenv("AZ")
-ZONE = os.getenv("ZONE")
-app_string = os.getenv("CF_Microservice")
-app_list = app_string.split(',')
-Chaos_Action = os.getenv("Chaos_Action")
-PASSWORD = os.getenv("PASSWORD")
+
+LATENCY = 1000
+DURATION = 30
+app_list = ["it-gb", "it-km-rest", "it-op-rest", "it-co", "it-app"]
+# Chaos_Action = "DELAY"
+Chaos_Action = "KILL"
+PASSWORD = "Prisminfra529#5"
 
 
 
@@ -48,6 +55,115 @@ def get_app_guid(token, app):
 
     return guid
 
+def get_zone():
+    z1 = []
+    z2 = []
+    z3 = []
+    zone_list = ["z1", "z2", "z3"]
+    app_zones = []
+    app_dict = {}  # https://stackoverflow.com/questions/23999801/creating-multiple-lists
+    token = cf_oauth_token()
+    # app_list = ["it-op-rest", "it-app"]
+
+    # print(f"No of apps selected is - {len(app_list)}")
+
+    for app in app_list:
+
+        app_dict["zones_of_" + app] = []  # https://stackoverflow.com/questions/23999801/creating-multiple-lists
+        guid = get_app_guid(token, app)
+
+        url = f"https://chaosmonkey.cf.sap.hana.ondemand.com/api/v1/apps/{guid}/mapping"
+
+        payload = {}
+
+        headers = {
+            'Authorization': 'Basic c2Jzc18rcGJnbmlvdjRpbGQxbGwydmp4dHVlbjl1dm90eXp6a3ZjdW1yenJtdG9jMXZnYmx6OWJlam11eDJtbGs2dXFncXYwPTphYV9EdFdCckRYL2ttdmY4cG5lR2diVHB4RDhHYTg9'
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        # print(response.json())
+
+        # print(f"No. of instances - {len(response.json()['mapping'])}")
+
+        app_z = response.json()
+
+        # print(app_z["mapping"]["0"]["name"])
+
+        for zone in range(0, len(response.json()["mapping"])):
+            i = str(zone)
+
+            # print(response.json()["mapping"][i]["name"])
+
+            azs = response.json()["mapping"][i]["name"]
+
+            # app_zones.append(azs)
+
+            app_dict["zones_of_" + app].append(azs)
+
+    # print(app_zones)
+
+    print(app_dict)
+
+    # TODO - list all the apps availble in the zones
+
+    # for app in app_list:
+    #     if "z1" in app_dict["zones_of_" + app]:
+    #         z1.append(app)
+    #     else:
+    #         continue
+    #     if "z2" in app_dict["zones_of_" + app]:
+    #         z2.append(app)
+    #     else:
+    #         continue
+    #     if "z3" in app_dict["zones_of_" + app]:
+    #         z3.append(app)
+    #
+    # print(f"\n{z1}\n{z2}\n{z3}\n")
+
+    for app in app_list:
+        if "z1" in app_dict["zones_of_" + app]:
+            z1.append(app)
+    for app in app_list:
+        if "z2" in app_dict["zones_of_" + app]:
+            z2.append(app)
+    for app in app_list:
+        if "z3" in app_dict["zones_of_" + app]:
+            z3.append(app)
+
+    print(f"\nz1={z1}\nz2={z2}\nz3={z3}\n")
+
+    # # TODO - REMOVING DUPLICATES - https://stackoverflow.com/questions/7961363/removing-duplicates-in-lists
+    #
+    # print("The final list after removing the duplicates, if any is as below- \n")
+    # print(f"z1 = {list(dict.fromkeys(z1))}")
+    # print(f"z2 = {list(dict.fromkeys(z2))}")
+    # print(f"z3 = {list(dict.fromkeys(z3))}")
+    #
+    #
+    # z1 = list(dict.fromkeys(z1))
+    # z2 = list(dict.fromkeys(z2))
+    # z3 = list(dict.fromkeys(z3))
+
+    # TODO - Selecting the zone where all MTMS are present.
+
+    if len(z1) == len(app_list):
+        print("we will select 'z1' for chaos action")
+        zone_to_be_used = "z1"
+        return zone_to_be_used
+    elif len(z2) == len(app_list):
+        print("we will select 'z2' for chaos action")
+        zone_to_be_used = "z2"
+        return zone_to_be_used
+    elif len(z3) == len(app_list):
+        print("we will select 'z3' for chaos action")
+        zone_to_be_used = "z3"
+        return zone_to_be_used
+    else:
+        zone_to_be_used = random.choice(zone_list)
+        print(zone_to_be_used)
+        return zone_to_be_used
+
 
 def crash(CF_Microservice):
     url = "https://chaosmonkey.cf.sap.hana.ondemand.com/api/v1/tasks"
@@ -55,9 +171,9 @@ def crash(CF_Microservice):
     payload = json.dumps({
         "app_name": CF_Microservice,
         "selector": {
-            "percentage": 100,
+            "percentage": 50,
             "azs": [
-                ZONE
+                get_zone()
             ]
         },
         "kind": "KILL",
@@ -81,10 +197,10 @@ def delay(CF_Microservice):
     payload = json.dumps({
         "app_name": CF_Microservice,
         "selector": {
-            "percentage": 100,
+            "percentage": 50,
 
             "azs": [
-                ZONE
+                get_zone()
             ]
         },
         "kind": "DELAY",
@@ -121,10 +237,29 @@ def execution_details(guid):
     print(json.dumps(result, indent=2))
 
 
+def app_state(token, app, guid):
+    url = f"https://api.cf.sap.hana.ondemand.com/v3/processes/{guid}/stats"
 
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    # print(response.json())
+
+    number_of_instances = len(response.json()["resources"])
+
+    print(f"Number of instances in '{app}' is  - '{number_of_instances}'")
+
+    for i in range(0, number_of_instances):
+        print(
+            f"Instance - {response.json()['resources'][i]['index']} of {app} is {response.json()['resources'][i]['state']}")
 
 if __name__ == '__main__':
     # app_list = ["it-gb", "it-km-rest", "it-op-rest", "it-co", "it-app"]
+
     t1 = time.time()
     p = Pool()
     if Chaos_Action == "DELAY":
@@ -138,8 +273,10 @@ if __name__ == '__main__':
 
     print("\n")
     print(f"this took: {time.time() - t1} ")
-    time.sleep(60)
+    time.sleep(120)
     token = cf_oauth_token()
     for app in app_list:
         guid = get_app_guid(token, app)
         execution_details(guid)
+        app_state(token,app,guid)
+
