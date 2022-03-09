e0d581722d2968cefa0acf3017f31f097994539e
Arpith-T <40532486+Arpith-T@users.noreply.github.com>
2022-03-09 07:54:30 +0530

    return the IST time

--- convert_utc_to_ist.py
+++ convert_utc_to_ist.py
@@ -15,7 +15,9 @@ def convert_utc_ist(utc):
 
 def main():
     utc_test_time = "2022-03-08T07:00:21.971538Z"
-    convert_utc_ist(utc=utc_test_time)
+    ist = convert_utc_ist(utc=utc_test_time)
+    print(f"The UTC time is {utc_test_time}")
+    print(f"The IST time is - {ist}")
 
 
 if __name__ == "__main__":
