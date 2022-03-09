e383caf1962b6717181ad88ce6ba66c239a160d7
Arpith-T <40532486+Arpith-T@users.noreply.github.com>
2022-03-09 07:43:08 +0530

    converting utc time to ist time - basically done by adding 5.30 hrs to the ist time. I need to figure out a better way if and when possible.

---
+++ convert_utc_to_ist.py
@@ -0,0 +1,21 @@
+from dateutil import parser
+import datetime
+
+
+def convert_utc_ist(utc):
+    # utc = '2022-03-08T07:00:21.971538Z'  # or any date string of differing formats.
+    utc_time = parser.parse(utc)
+    # print(utc_time)
+    hours = 5.30
+    hours_added = datetime.timedelta(hours=hours)
+    ist_time = utc_time + hours_added
+    print(ist_time)
+
+
+def main():
+    utc_test_time = "2022-03-08T07:00:21.971538Z"
+    convert_utc_ist(utc=utc_test_time)
+
+
+if __name__ == "__main__":
+    main()
